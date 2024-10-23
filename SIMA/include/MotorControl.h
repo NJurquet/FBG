3e2d0b0d4ea1cec15efc639e3464c4eef58cfd54
NJurquet <nicolor01@hotmail.com>
2024-10-23 14:35:03 +0200

    Add function & class documentation

--- SIMA/include/MotorControl.h
+++ SIMA/include/MotorControl.h
@@ -1,12 +1,37 @@
 #ifndef MOTORCONTROL_H
 #define MOTORCONTROL_H
 
+/**
+ * @namespace MotorControl
+ * @brief Namespace for motor control functions.
+ */
 namespace MotorControl
 {
+    /**
+     * @brief Initializes the motor control system.
+     *
+     * This method sets up the necessary configurations for the Motors and Motor Shield.
+     */
     void init();
+
+    /**
+     * @brief Moves the robot forward.
+     */
     void moveForward();
+
+    /**
+     * @brief Rotates the robot to the left.
+     */
     void rotateLeft();
+
+    /**
+     * @brief Rotates the robot to the right.
+     */
     void rotateRight();
+
+    /**
+     * @brief Stops the robot.
+     */
     void stop();
 }
 
