dad17a3a5bd2206ae0b8387396fea3ea9e1d4f6f
Dori-Tos <dorian.smaelen@hotmail.com>
2025-02-11 15:13:33 +0100

    Comment strategy in main big bot fsm

--- BIG_BOT/FSM.h
+++ BIG_BOT/FSM.h
@@ -1,6 +1,19 @@
 #ifndef FSM_H
 #define FSM_H
 
+// Decision making algorithm:
+
+// 1. Detect the targets (drop area, cans, planks)
+// 2. Plan the route => Most efficient route + oriented perpendicular to the planks
+//                      + If possible : avoid adversary (if balise placed on top)
+// 3. Standard movement FSM
+// 4. Always check for obstacles => if any => avoid mode (to determine)
+// 5. Precise movement to collect the cans + FSM to collect the cans
+// 6. Slow movement to push the planks and cans to target area
+// 7. Precise movement to drop the cans + FSM to drop the cans and planks
+// 8. Repeat until time is up (margin of 10? seconds) => return to starting area
+// 9. Stop the motors + Party time
+
 class FSM
 {
 public:
@@ -22,6 +35,7 @@ private:
         COLLECT,            // Uses its own FSM (how to avoid ?)
         SLOW_MOVE,                  // May be redundant but used for
         SLOW_ROTATE,                // deplacement when pushing the planks and cans
+        MOVE_TO_DROP,
         DROP,               // Uses its own FSM (how to avoid ?)
         RETURN,
         STOP
