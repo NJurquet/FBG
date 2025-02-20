f35f8a1aced19a7c5cb67e7a1d15b135be91e4c5
NJurquet <nicolor01@hotmail.com>
2025-02-20 20:46:26 +0100

    Add constants.py & config.py in BigBot

---
+++ BIG_BOT/constants.py
@@ -0,0 +1,9 @@
+"""
+constants.py
+
+This module contains all constants used throughout the project.
+
+THIS FILE SHOULD NOT BE MODIFIED EXCEPT FOR ADDING NEW CONSTANTS. ALL CONFIGURATION SHOULD BE DONE IN config.py.
+"""
+
+from enum import Enum
