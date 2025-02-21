63ba4b7af637f9735151abbc0786152059dde1e4
Dori-Tos <21006@ecam.be>
2025-02-21 13:24:47 +0100

    Implement motor control prototype

--- BIG_BOT/motor.py
+++ BIG_BOT/motor.py
@@ -1,13 +1,8 @@
 from gpiozero import Motor
 from time import sleep
-motor1 = Motor(forward=4, backward=14) #Pin numbers
+motor1 = Motor(17,27) #Pin numbers
 
-motor1.forward()
-test = 0 
-while test <3:
-    sleep(5)
-    motor1.reverse()
-    
-    test +=1
+while True:
+    motor1.forward()
+    print("T")
 
-motor1.stop()
