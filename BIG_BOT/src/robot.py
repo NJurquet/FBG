7cc05d7dac18d0bdc427eff0fd1e840e2b7f3db6
NJurquet <nicolor01@hotmail.com>
2025-02-27 21:12:27 +0100

    Document Robot class

--- BIG_BOT/src/robot.py
+++ BIG_BOT/src/robot.py
@@ -4,19 +4,44 @@ from .hardware.motorsControl import MotorsControl as Motors
 
 
 class Robot:
+    """
+    Class representing the robot, including its Finite State Machine (FSM), hardware components and characteristics.
+    """
+
     def __init__(self):
         self.fsm = RobotFSM(self)
         self.motor = Motors(LEFT_MOTOR_FORWARD_PIN, LEFT_MOTOR_BACKWARD_PIN, RIGHT_MOTOR_FORWARD_PIN, RIGHT_MOTOR_BACKWARD_PIN)
-        # self.camera = None
+        self.camera = None
 
         self.__position: tuple[int, int] = (0, 0)
 
     @property
     def position(self):
+        """
+        Getter for the position of the robot.
+
+        Returns
+        -------
+        tuple[int, int]
+            The x and y coordinates of the robot.
+        """
         return self.__position
 
     @position.setter
     def position(self, value: tuple[int, int]):
+        """
+        Setter for the position of the robot.
+
+        Parameters
+        ----------
+        value : tuple[int, int]
+            The new x and y coordinates of the robot.
+
+        Raises
+        ------
+        ValueError
+            If the position is not a tuple of two integers.
+        """
         if isinstance(value, tuple) and len(value) == 2:
             self.__position = value
         else:
