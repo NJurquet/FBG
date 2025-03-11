fd5b299067cb231bc2d8a4232bcf3f54438923d4
Yessboom <Tanguy.guissard@hotmail.be>
2025-03-11 13:12:56 +0100

    Add reedSwitch class for magnetic switch functionality

---
+++ BIG_BOT/src/hardware/reedSwitch.py
@@ -0,0 +1,11 @@
+from gpiozero import Button
+
+#Class for the magnetic switch used to start the robot
+class reedSwitch:
+    def __init__(self, pin):
+        self.pin = pin
+        self.reedSwitch = Button(pin, pull_up=True)
+    
+    def read(self):
+        return self.reedSwitch.is_pressed
+    
