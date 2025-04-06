d0ccbc2f3d5372c4c12a967d3b2d1b77ded391ca
NJurquet <nicolor01@hotmail.com>
2025-04-06 11:55:27 +0200

    Adds default score and LCD integration to code
    Introduces a default score constant.
    Accepts a score parameter and writes the score on the LCD in the robot initializer.

--- BIG_BOT/src/robot.py
+++ BIG_BOT/src/robot.py
@@ -2,10 +2,12 @@ from .config import LEFT_MOTOR_FORWARD_PIN, LEFT_MOTOR_BACKWARD_PIN, LEFT_MOTOR_
 from .config import US_FRONT_RIGHT_TRIG_PIN, US_FRONT_RIGHT_ECHO_PIN, US_FRONT_LEFT_TRIG_PIN, US_FRONT_LEFT_ECHO_PIN, US_BACK_RIGHT_TRIG_PIN, US_BACK_RIGHT_ECHO_PIN, US_BACK_LEFT_TRIG_PIN, US_BACK_LEFT_ECHO_PIN
 from .config import SERVO_CHANNELS
 from .config import REED_SWITCH_PIN, CENTER_RIGHT_CLAW_NAME, CENTER_RIGHT_CLAW_ADAFRUIT_PIN
+from .config import DEFAULT_SCORE
 from .constants import USPosition
 from .fsm.FSM import RobotFSM
 from .hardware.motorsControl import MotorsControl as Motors
 from .hardware.servoControl import ServoControl
+from .hardware.lcd import LCD
 from .hardware.adafruitServoController import AdafruitServoControl
 from .hardware.ultrasonicController import UltrasonicController
 from .hardware.reedSwitch import reedSwitch
@@ -16,23 +18,26 @@ class Robot:
     Class representing the robot, including its Finite State Machine (FSM), hardware components and characteristics.
     """
 
-    def __init__(self):
+    def __init__(self, score: int = DEFAULT_SCORE):
         self.fsm = RobotFSM(self)
         self.motor = Motors(LEFT_MOTOR_FORWARD_PIN, LEFT_MOTOR_BACKWARD_PIN, LEFT_MOTOR_EN_PIN,
                             RIGHT_MOTOR_FORWARD_PIN, RIGHT_MOTOR_BACKWARD_PIN, RIGHT_MOTOR_EN_PIN)
         # self.servoControl = ServoControl([CENTER_RIGHT_CLAW_NAME], [CENTER_RIGHT_CLAW_PIN])
-        # self.servoControl = AdafruitServoControl(channels=SERVO_CHANNELS,
-                                                #  names=[CENTER_RIGHT_CLAW_NAME],
-                                                #  pins=[CENTER_RIGHT_CLAW_ADAFRUIT_PIN])
+        # self.servoControl = AdafruitServoControl(channels=SERVO_CHANNELS, names=[
+        #                                          CENTER_RIGHT_CLAW_NAME],
+        #                                          pins=[CENTER_RIGHT_CLAW_ADAFRUIT_PIN])
+        self.lcd = LCD()
         self.camera = None
         self.ultrasonicController = UltrasonicController()
         self.ultrasonicController.add_sensor(USPosition.FRONT_RIGHT, US_FRONT_RIGHT_ECHO_PIN, US_FRONT_RIGHT_TRIG_PIN)
         self.ultrasonicController.add_sensor(USPosition.FRONT_LEFT, US_FRONT_LEFT_ECHO_PIN, US_FRONT_LEFT_TRIG_PIN)
         # self.ultrasonicController.add_sensor(USPosition.BACK_RIGHT, US_BACK_RIGHT_ECHO_PIN, US_BACK_RIGHT_TRIG_PIN)
         # self.ultrasonicController.add_sensor(USPosition.BACK_LEFT, US_BACK_LEFT_ECHO_PIN, US_BACK_LEFT_TRIG_PIN)
-
-        self.__position: tuple[int, int] = (0, 0)
         # self.reedSwitch = reedSwitch(REED_SWITCH_PIN)
+
+        self.score = score
+        self.lcd.write_score(self.score)
+        self.__position: tuple[int, int] = (0, 0)
 
     @property
     def position(self):
