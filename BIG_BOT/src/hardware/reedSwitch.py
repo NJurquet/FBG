7543f8473dfca4627973cab19fbc6e32cbcb50cb
Yessboom <Tanguy.guissard@hotmail.be>
2025-04-07 10:07:01 +0200

    Delete first testing file

--- BIG_BOT/src/hardware/reedSwitch.py
+++ BIG_BOT/src/hardware/reedSwitch.py
@@ -2,9 +2,27 @@ from gpiozero import Button
 
 #Class for the magnetic switch used to start the robot
 class reedSwitch:
+    """
+    Class representing a reed switch, use to start our robot.
+
+    Parameters
+    ----------
+    `pin` : int
+        The GPIO pin number to which the reed switch is connected.
+
+    
+    Methods
+    -------
+    `read()` : int
+        Returns the state of the reed switch (0 for not pressed, 1 for pressed).
+    
+    `long_read()` : int
+        Waits for the reed switch to be pressed for a certain duration (10 checks) and returns 1 if pressed, 0 otherwise.
+
+    """
     def __init__(self, pin: int):
         self.pin = pin
-        self.reedSwitch = Button(pin, pull_up=True)
+        self.reedSwitch = Button(pin, pull_up=False)
     
     def read(self):
         return self.reedSwitch.is_pressed
