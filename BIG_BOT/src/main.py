cc1bec8b48856ea105835689b6b7c64bb431cbb1
NJurquet <nicolor01@hotmail.com>
2025-04-08 09:05:28 +0200

    Adds CLI color argument
    Integrates new command line argument for color selection with defined choices of the zones.
    Updates robot initialization to save color assignment for flexible game configuration.

--- BIG_BOT/src/main.py
+++ BIG_BOT/src/main.py
@@ -3,12 +3,14 @@ from .robot import Robot
 
 
 def main():
-    parser = argparse.ArgumentParser(usage="python -m BIG_BOT.src.main [-h] --score SCORE",
+    parser = argparse.ArgumentParser(usage="python -m BIG_BOT.src.main [-h] --color COLOR--score SCORE ",
                                      description="Run the robot and set the expected score displayed on the LCD.")
     parser.add_argument("--score", type=int, required=True, help="The expected score displayed on the LCD.")
+    parser.add_argument("--color", type=str, required=True, choices=["yellow", "blue"],
+                        help="The color assigned to the robot for the game.")
     args = parser.parse_args()
 
-    robot: Robot = Robot(args.score)
+    robot: Robot = Robot(args.color, args.score)
 
     while True:
         robot.fsm.update()
