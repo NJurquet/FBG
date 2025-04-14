dab4417d0aa9cade414758fb11ce4ebeea77d279
Yessboom <Tanguy.guissard@hotmail.be>
2025-04-14 09:48:52 +0200

    Add enable and Disable sensors command to improve code readability

--- BIG_BOT/src/fsm/commands/ultrasonicCommands.py
+++ BIG_BOT/src/fsm/commands/ultrasonicCommands.py
@@ -6,7 +6,7 @@ if TYPE_CHECKING:
     from ..FSM import RobotFSM
 
 class ToggleUltrasonicSensorsCommand(ICommand):
-    def __init__(self, fsm: 'RobotFSM', positions: list[USPosition]):
+    def __init__(self, fsm: 'RobotFSM', positions: list[USPosition],):
         self.fsm = fsm
         self.initialStates = fsm.robot.ultrasonicController.get_enabled_sensors()
         self.positions = positions
@@ -35,3 +35,66 @@ class ToggleUltrasonicSensorsCommand(ICommand):
 
     def finished(self):
         pass  # No action needed for finished
+
+class  DisableUltrasonicSensorsCommand(ICommand):
+    def __init__(self, fsm: 'RobotFSM', positions: list[USPosition],):
+        self.fsm = fsm
+        self.initialStates = fsm.robot.ultrasonicController.get_enabled_sensors()
+        self.positions = positions
+
+    def execute(self):
+        for i in range(len(self.positions)):
+            self.fsm.robot.ultrasonicController.disable_sensor(self.positions[i])
+
+    def pause(self):
+        pass  # No action needed for pause
+
+    def resume(self):
+        current_states = self.fsm.robot.ultrasonicController.get_enabled_sensors()
+    
+        for position in self.positions:
+            # Check if the current state is different from the initial state
+            current_state = current_states[position]
+            initial_state = self.initialStates[position]
+
+            if current_state == initial_state:
+                # Toggle the neeeded sensor to the opposite state
+                self.fsm.robot.ultrasonicController.toggle_sensor(position)
+
+    def stop(self):
+        pass  # No action needed for stop
+
+    def finished(self):
+        pass  # No action needed for finished
+
+class EnableUltrasonicSensorsCommand(ICommand):
+    def __init__(self, fsm: 'RobotFSM', positions: list[USPosition],):
+        self.fsm = fsm
+        self.initialStates = fsm.robot.ultrasonicController.get_enabled_sensors()
+        self.positions = positions
+
+    def execute(self):
+        for i in range(len(self.positions)):
+            self.fsm.robot.ultrasonicController.enable_sensor(self.positions[i])
+
+    def pause(self):
+        pass  # No action needed for pause
+
+    def resume(self):
+        current_states = self.fsm.robot.ultrasonicController.get_enabled_sensors()
+    
+        for position in self.positions:
+            # Check if the current state is different from the initial state
+            current_state = current_states[position]
+            initial_state = self.initialStates[position]
+
+            if current_state == initial_state:
+                # Toggle the neeeded sensor to the opposite state
+                self.fsm.robot.ultrasonicController.toggle_sensor(position)
+
+    def stop(self):
+        pass  # No action needed for stop
+
+    def finished(self):
+        pass  # No action needed for finished
+
